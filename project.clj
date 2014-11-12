76d6fb43019ff4b03469859a0850b1939f6e4235
alexpetrov <alexpetrov.rb@gmail.com>
2014-11-13 00:00:52 +0300

    Initial commit

---
+++ project.clj
@@ -0,0 +1,26 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.8"]
+            [com.cemerick/austin "0.1.5"]]
+  :ring {:handler modern-cljs.core/handler}
+  :cljsbuild {:builds
+              [{;; CLJS source code path
+                :source-paths ["src/cljs"]
+
+                ;; Google Closure (CLS) options configuration
+                :compiler {;; CLS generated JS script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal JS optimization directive
+                           :optimizations :whitespace
+
+                           ;; generated JS code prettyfication
+                           :pretty-print true}}]})
